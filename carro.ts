f969771b05752311d3f364770c25bc84e68d8e70
casagrande7 <joaofootz10@gmail.com>
2023-10-03 09:30:57 -0300

    Atualizado

--- carro.ts
+++ carro.ts
@@ -11,3 +11,21 @@ class CarroCompacto implements Carro{
         console.log("O carro compacto freou.");
     }
 }
+
+class CarrosEsportivo implements Carro{
+    acelerar(velocidade: number): void {
+        console.log("O carro esportivo acelerou para" +  velocidade + "Km/h");
+    }
+
+    frear(): void {
+        console.log("O carro esportivo freou.");
+    }
+}
+
+const meuCarroEsportivo = new CarrosEsportivo();
+meuCarroEsportivo.acelerar(200);
+meuCarroEsportivo.frear();
+
+const meuCarroCompacto = new CarroCompacto();
+meuCarroCompacto.acelerar(120);
+meuCarroCompacto.frear();
